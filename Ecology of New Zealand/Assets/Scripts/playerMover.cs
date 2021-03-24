0e2580c7636d0a807e367e57fb42d9d9b9343d1b
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2021-03-24 17:56:58 +1300

    Redid character
    Better camera, disabled jump (might put it back on later if required)

--- Ecology of New Zealand/Assets/Scripts/playerMover.cs
+++ Ecology of New Zealand/Assets/Scripts/playerMover.cs
@@ -5,94 +5,88 @@ using UnityEngine;
 public class playerMover : MonoBehaviour
 {
     // Start is called before the first frame update
-    Rigidbody rb;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float runSpeed;
-    [SerializeField] private float jumpHeight;
+    public Camera MyCamera;
+    [SerializeField] float Run = 4f;
+    [SerializeField] float Walk = 2f;
+    bool Walking;
 
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private LayerMask groundMask;
-    [SerializeField] private float gravity;
 
-    private Vector3 moveDirection;
-    private Vector3 velocity;
+    float desiredRotation = 0f;
+    float desiredAnimationSpeed = 0f;
+    float RotationSpeed = 15f;
+    float AnimationBlendSpeed = 6f;
+    float jumpSpeed = 0f;  //Jumping is disabled
+    float SpeedY = 0;
+    float gravity = -9.81f;
 
+
+    Animator MyAnimator;
 
     private CharacterController controller;
-
+    bool jumping = false;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        MyAnimator = GetComponent<Animator>();
 
     }
     private void Update()
     {
-        Move();
-    }
 
-    private void Move()
-    {
-        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
-        if (isGrounded && velocity.y < 0)
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        if (Input.GetButtonDown("Jump") && !jumping)
         {
-            velocity.y = -2f;
+            jumping = true;
+            MyAnimator.SetTrigger("Jump");
+            SpeedY += jumpSpeed;
+        } 
+        if (!controller.isGrounded) {
+        SpeedY += gravity * Time.deltaTime;
         }
-
-        float moveZ = Input.GetAxis("Vertical");
-        moveDirection = new Vector3(0, 0, -moveZ);
-
-        if (isGrounded)
+        else if (SpeedY < 0)
         {
-
-            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
-            {
-                Walk();
-            }
-            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
-            {
-                Run();
-            }
-            else if (moveDirection == Vector3.zero)
-            {
-                Idle();
-            }
-            moveDirection *= moveSpeed;
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Jump();
-            }
-
+            SpeedY = 0;
         }
-        controller.Move(moveDirection * Time.deltaTime);
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        MyAnimator.SetFloat("SpeedY", SpeedY/jumpSpeed);
+
+        if (jumping && SpeedY < 0)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, .5f, LayerMask.GetMask("Ground")))
+            {
+                jumping = false;
+                MyAnimator.SetTrigger("Land");
+            }
+        }
+        Walking = Input.GetKey(KeyCode.LeftShift);
+
+        Vector3 movement = new Vector3(x, 0, z).normalized;
+
+        Vector3 rotatedMovement = Quaternion.Euler(0, MyCamera.transform.rotation.eulerAngles.y,0) * movement;
+        Vector3 verticalMovement = Vector3.up * SpeedY;
+
+
+        controller.Move((verticalMovement + (rotatedMovement * (Walking ? Walk : Run) * Time.deltaTime)));
+
+        if (rotatedMovement.magnitude > 0)
+        {
+            desiredRotation = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg;
+            desiredAnimationSpeed = Walking ? .5f : 1f;
+        }
+        else
+        {
+            desiredAnimationSpeed = 0;
+        }
+        MyAnimator.SetFloat("Speed", Mathf.Lerp(MyAnimator.GetFloat("Speed"),desiredAnimationSpeed, AnimationBlendSpeed *Time.deltaTime));
+
+        Quaternion currentRotation = transform.rotation;
+        Quaternion targetRotation  = Quaternion.Euler(0, desiredRotation, 0);
+        transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, RotationSpeed * Time.deltaTime);
+
+
     }
 
-
-
-    void Idle()
-    {
-
-    }
-
-    void Walk()
-    {
-        moveSpeed = walkSpeed;
-
-    }
-
-    void Run()
-    {
-        moveSpeed = runSpeed;
-    }
-
-    void Jump()
-    {
-        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-    }
 
 }
